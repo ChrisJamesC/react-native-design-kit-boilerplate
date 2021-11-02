668e44c2a1d88e6b4218a9bed553b7b6516c7b84
Christopher Chiche <christopher.chiche@gmail.com>
2021-11-02 10:52:54 +0100

    add publish action

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -8,58 +8,53 @@ interface MyButtonProps {
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingHorizontal: 16,
     paddingVertical: 12,
-    position: "relative",
+    position: 'relative',
     width: 352,
     height: 48,
 
     /* Light/Neutral/G1000 */
 
-    backgroundColor: "#000000",
+    backgroundColor: '#000000',
     borderRadius: 24,
-
-
   },
   text: {
     /* label */
 
-
     // position: "static",
     width: 39,
-    left: "calc(50% - 39px/2)",
+    left: 'calc(50% - 39px/2)',
     top: 0,
     bottom: 0,
 
     /* ButtonLabel/Default */
 
-    fontFamily: "Lab Grotesque",
-    fontStyle: "normal",
-    fontWeight: "normal",
+    fontFamily: 'Lab Grotesque',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
     fontSize: 16,
     lineHeight: 24,
     /* identical to box height, or 150% */
 
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
 
     /* Light/Neutral/G000 */
 
-    color: "#FFFFFF",
-
+    color: '#FFFFFF',
 
     /* Inside Auto Layout */
 
     // flex: "none",
     // order: 1,
     flexGrow: 0,
-    margin: "0px 8px",
+    margin: '0px 8px',
   },
-
 });
 
 const MyButton = ({ onPress, text }: MyButtonProps) => (
