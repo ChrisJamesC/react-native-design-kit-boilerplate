6583ca97ebaf6c045d6d8132d27f10f11de6ebf6
Christopher Chiche <christopher.chiche@gmail.com>
2021-11-03 22:01:07 +0100

    explicit mention of react-native for storyshots

--- Storyshots.test.ts
+++ Storyshots.test.ts
@@ -3,7 +3,7 @@ import path from "path";
 import initStoryshots from "@storybook/addon-storyshots";
 
 initStoryshots({
-  // framework: "react-native", // Manually specify the project's framework
+  framework: "react-native", // Manually specify the project's framework
   configPath: path.join(__dirname, ".storybook/storybook.requires.js"),
   // integrityOptions: { cwd: path.join(__dirname, "src", "stories") },
   // Other configurations
