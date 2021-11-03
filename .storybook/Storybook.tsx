e6bd79360db405ccbb0ff7f4b6e2c7f6505cacf0
Christopher Chiche <christopher.chiche@gmail.com>
2021-11-03 22:14:29 +0100

    Follow storybook instructions

--- .storybook/Storybook.tsx
+++ .storybook/Storybook.tsx
@@ -1,10 +1,6 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getStorybookUI } from '@storybook/react-native';
-
 import './storybook.requires';
 
-const StorybookUIRoot = getStorybookUI({
-  asyncStorage: AsyncStorage,
-});
+const StorybookUIRoot = getStorybookUI();
 
 export default StorybookUIRoot;
