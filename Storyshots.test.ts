ec505b4c51bc403bc3c199842eff9c14936e7c35
Christopher Chiche <christopher.chiche@gmail.com>
2021-11-05 22:36:31 +0100

    Add debug info

--- Storyshots.test.ts
+++ Storyshots.test.ts
@@ -2,9 +2,11 @@ import path from "path";
 
 import initStoryshots from "@storybook/addon-storyshots";
 
+console.log(">>>>>> dirname", __dirname);
+
 initStoryshots({
   framework: "react-native", // Manually specify the project's framework
   configPath: path.join(__dirname, ".storybook/storybook.requires.js"),
-  // integrityOptions: { cwd: path.join(__dirname, "src", "stories") },
+  integrityOptions: { cwd: path.join(__dirname, "src", "stories") },
   // Other configurations
 });
