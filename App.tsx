de121d6c0929e930f848ea2466ca8486516b4387
Christopher Chiche <christopher.chiche@gmail.com>
2021-11-15 16:25:29 +0100

    fix main path to stories

--- App.tsx
+++ App.tsx
@@ -1,8 +1,3 @@
 import React from 'react';
 import StorybookUIRoot from './.storybook/Storybook';
 export { StorybookUIRoot as default };
-// const App = () => {
-//   <StorybookUIRoot />;
-// };
-
-// export default App;
